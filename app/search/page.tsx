3fb15f169b593bdbf8c6e1ccdffc6aff63dec0b9
fares <fereshouidi298@gmail.com>
2025-10-27 15:20:18 +0100

    edit

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,71 +1,10 @@
-"use client";
-import { backEndUrl } from '@/api';
-import FilterBar from '@/componnent/main/filterBar';
-import Footer from '@/componnent/main/footer';
-import Header from '@/componnent/main/header';
-import SideBar from '@/componnent/main/sideBar';
-import { useScreen } from '@/contexts/screenProvider';
-import { useTheme } from '@/contexts/themeProvider';
-import { OwnerInfoType, PubType } from '@/types';
-import axios from 'axios';
-import { useSearchParams } from 'next/navigation'
-import React, { Suspense, useEffect, useState } from 'react'
+import { Suspense } from 'react'
+import SearchClient from './SearchClient'
 
 export default function Page() {
-
-  const params = useSearchParams();
-  const collectionId = params.get('collectionId');
-  const searchInput = params.get('searchInput');
-  const [sideBarActive, setSideBarActive] = useState<boolean>(false);
-  const { colors } = useTheme();
-  const [pub, setPub] = useState<PubType | undefined>(undefined);
-  const { screenWidth } = useScreen();
-  const [ownerInfo, setOwnerInfo] = useState<OwnerInfoType | undefined>(undefined);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await axios.get(backEndUrl + "/getOwnerInfo")
-      .then(({ data }) => setOwnerInfo(data.ownerInfo))
-      .catch((err) => {
-        throw err
-      })
-    }
-    fetchData();
-  }, [])
-
-  useEffect(() => {
-    console.log({ownerInfo});
-    
-  }, [ownerInfo])
-  
   return (
     <Suspense fallback={<p>Loading search results...</p>}>
-      <div>
-        <Header
-          isSideBarActive={sideBarActive}
-          setIsSideBarActive={setSideBarActive}
-          ownerInfo={ownerInfo}
-          setOwnerInfo={setOwnerInfo}
-        />
-
-        <FilterBar/>
-
-        <div className='w-full min-h-screen'>
-          {collectionId}
-          <p>{searchInput}</p>
-        </div>
-
-        <Footer/>
-
-          
-        <SideBar
-          isActive={sideBarActive}
-          setIsActive={setSideBarActive}
-          ownerInfo={ownerInfo}
-          setOwnerInfo={setOwnerInfo}
-        />
-
-      </div>    
+      <SearchClient />
     </Suspense>
   )
 }
