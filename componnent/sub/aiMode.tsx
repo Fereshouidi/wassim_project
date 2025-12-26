4ce73f0cf812fbb37ff00e05ce827d750e141cef
fares <fereshouidi298@gmail.com>
2025-12-26 22:26:16 +0100

    edit

--- componnent/sub/aiMode.tsx
+++ componnent/sub/aiMode.tsx
@@ -22,67 +22,67 @@ const AiMode = ({
     
     // activeTheme == "dark" ? "/icons/ai_black.png" : "/icons/ai_black.png" 
     
-    return (
+    return (<div></div>
 
-        <div 
-            className={`${aiModeHover || aiModeActive ? 'w-[100px]' : 'w-[50px]'} h-[80%] flex justify-center items-center relative overflow-hidden m-3 rounded-full transition-[width] duration-300`}
-            onClick={() => {
-                setAiModeActive(!aiModeActive)
-                setAiModeHover(aiModeActive ? false : aiModeHover)
-            }}
-        >
+        // <div 
+        //     className={`${aiModeHover || aiModeActive ? 'w-[100px]' : 'w-[50px]'} h-[80%] flex justify-center items-center relative overflow-hidden m-3 rounded-full transition-[width] duration-300`}
+        //     onClick={() => {
+        //         setAiModeActive(!aiModeActive)
+        //         setAiModeHover(aiModeActive ? false : aiModeHover)
+        //     }}
+        // >
 
-            <div    
-                className={`${aiModeHover || aiModeActive ? 'w-[97px] p-3' : 'w-[47px] p-2'} h-[93%] flex justify-center items-center rounded-full z-10 overflow-hidden relative transition-[width] duration-300`}
-                style={{
-                    ...aiIconStyle,
-                    // backgroundColor: colors.light[200]
+        //     <div    
+        //         className={`${aiModeHover || aiModeActive ? 'w-[97px] p-3' : 'w-[47px] p-2'} h-[93%] flex justify-center items-center rounded-full z-10 overflow-hidden relative transition-[width] duration-300`}
+        //         style={{
+        //             ...aiIconStyle,
+        //             // backgroundColor: colors.light[200]
                     
-                }}
-                onClick={() => setAiModeActive(!aiModeActive)}
-                onMouseEnter={() => setAiModeHover(true)}
-                onMouseLeave={() => setAiModeHover(false)}
-            >
-                <h4 
-                    className={`
-                        text-sm ${aiModeHover || aiModeActive ? "opacity-100" : "opacity-0 invisible w-0"} 
-                        z-10 transition-[width] duration-300- whitespace-nowrap block text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-red-500
-                    `}
-                    style={{
-                        // color: colors.dark[400]
-                        ...aiIconContentStyle
-                    }}
-                >Ai mode</h4>
+        //         }}
+        //         onClick={() => setAiModeActive(!aiModeActive)}
+        //         onMouseEnter={() => setAiModeHover(true)}
+        //         onMouseLeave={() => setAiModeHover(false)}
+        //     >
+        //         <h4 
+        //             className={`
+        //                 text-sm ${aiModeHover || aiModeActive ? "opacity-100" : "opacity-0 invisible w-0"} 
+        //                 z-10 transition-[width] duration-300- whitespace-nowrap block text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-red-500
+        //             `}
+        //             style={{
+        //                 // color: colors.dark[400]
+        //                 ...aiIconContentStyle
+        //             }}
+        //         >Ai mode</h4>
 
-                <img 
-                    className='rounded-sm cursor-pointer mx-2 h-[80%] z-10'
-                    src={aiIcon} 
-                    alt="" 
-                    style={{
-                        // backgroundColor: colors.dark[100]
-                        // ...searchIconStyle,
-                    }}
-                />
+        //         <img 
+        //             className='rounded-sm cursor-pointer mx-2 h-[80%] z-10'
+        //             src={aiIcon} 
+        //             alt="" 
+        //             style={{
+        //                 // backgroundColor: colors.dark[100]
+        //                 // ...searchIconStyle,
+        //             }}
+        //         />
 
-            </div>
+        //     </div>
         
 
-            <video 
-                src={"/AIBg.webm"}
-                autoPlay={true}
-                loop={true}
-                className={`absolute w-[500%] h-[500%] object-cover pointer-events-none opacity-100 transition-[width] duration-300 z-0`}
-            ></video>
+        //     <video 
+        //         src={"/AIBg.webm"}
+        //         autoPlay={true}
+        //         loop={true}
+        //         className={`absolute w-[500%] h-[500%] object-cover pointer-events-none opacity-100 transition-[width] duration-300 z-0`}
+        //     ></video>
 
-                        {/* <DotLottieReact
-                src={"/AIBg.json"}
-                autoplay
-                loop
-                // muted
-                className={`absolute w-[500%] h-[500%] object-cover pointer-events-none opacity-100 transition-[width] duration-300 z-0`}
-            /> */}
+        //                 {/* <DotLottieReact
+        //         src={"/AIBg.json"}
+        //         autoplay
+        //         loop
+        //         // muted
+        //         className={`absolute w-[500%] h-[500%] object-cover pointer-events-none opacity-100 transition-[width] duration-300 z-0`}
+        //     /> */}
         
-        </div>
+        // </div>
     )
 }
 
